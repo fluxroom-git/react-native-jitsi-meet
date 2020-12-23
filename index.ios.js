b6806c36cfd8d30301dc2d104aa58b3ad1099bd6
manav-kasare <kmanav0705@gmail.com>
2020-12-24 05:04:23 +0530

    Some Changes

--- index.ios.js
+++ index.ios.js
@@ -2,20 +2,18 @@
  * @providesModule JitsiMeet
  */
 
-import { NativeModules, requireNativeComponent } from 'react-native';
+import { NativeModules, requireNativeComponent } from "react-native";
 
-export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
+export const JitsiMeetView = requireNativeComponent("RNJitsiMeetView");
 export const JitsiMeetModule = NativeModules.RNJitsiMeetView;
 const call = JitsiMeetModule.call;
 const audioCall = JitsiMeetModule.audioCall;
-JitsiMeetModule.call = (url, userInfo) => {
+JitsiMeetModule.call = (url, userInfo, meetOptions, meetFeatureFlags) => {
   userInfo = userInfo || {};
-  call(url, userInfo);
-}
+  call(url, userInfo, meetOptions, meetFeatureFlags);
+};
 JitsiMeetModule.audioCall = (url, userInfo) => {
   userInfo = userInfo || {};
   audioCall(url, userInfo);
-}
+};
 export default JitsiMeetModule;
-
-
